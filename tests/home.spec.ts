4b4611c6a53bca33048cca70d969a013a35a2757
kseniia <kseniya.vozisova@gmail.com>
2025-02-08 16:50:45 +0100

    refactor tests
    -improve readability of tests by separating each of them

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,21 +1,30 @@
 import { test, expect} from "@playwright/test";
 
-test("Home page", async ({ page }) => {
+test.describe("Home page", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("http://practicesoftwaretesting.com/");
+    })
+    test("Validate signing in", async ({ page }) => {
     await page.goto("http://practicesoftwaretesting.com/");
     // Ensure the sind in link is present
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+});
 
-    //Check the title of the page
+test("Validate the title of the page", async ({ page }) => {
     await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+});
 
-    //Check the count of items displayed on the page
+test("Validate loading of 9 items displayed on the page", async ({ page }) => {
     const productGrid = page.locator(".col-md-9");
     await page.locator('.card.skeleton').first().waitFor({ state: 'detached', timeout: 10000 });
     await expect(productGrid.getByRole("link")).toHaveCount(9);
+});
 
-    //Search for "Thor Hammer" and check the result
+test("Validate that Slip Joint Pliers persist", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
     await page.getByTestId("search-query").fill("Slip Joint Pliers");
     await page.getByTestId("search-submit").click();
     await expect(productGrid.getByRole("link")).toHaveCount(1);
     await expect(page.getByAltText("Slip Joint Pliers")).toBeVisible();
 });
+})
